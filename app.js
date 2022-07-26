21c504ae60ea2d5e0990508176df702737c0db7f
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2022-07-26 22:39:58 +0200

    npm reinstalled && /profile page added

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require('express', '4.18.1');
 const bodyParser = require('body-parser');
-
 const app = express();
+const https = require('https');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("views"));
 
-app.get('/', function(req, res) 
-{
+app.get('/', function(req, res) {
     res.render("index.ejs");
 });
 
-app.listen(process.env.PORT || 3000, function() 
-{
+app.get('/profile', function(req, res) {
+    res.render("profile.ejs");
+});
+
+app.post("/", function(req, res) {
+    const searchInput = req.body.searchInput;
+    res.render(searchInput);
+});
+
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server online");
 }); 
