064686b34f94acb44bb734b57f3c38d8087f5dea
LudwigLouisLentfort <linus.luedeke@onine.de>
2022-08-02 11:37:11 +0200

    api stuff + sorting

--- views/js/home.js
+++ views/js/home.js
@@ -1,7 +1,7 @@
 var htmlcode =
   "<div id=\"carouselExampleControls\" class=\"carousel slide\" data-bs-ride=\"carousel\"><div class=\"carousel-inner\">";
 
-  http.get( "http://localhost:4242/item/", function(responds) 
+  http.get( "http://localhost:4242/item/", function(responds)
   {
       responds.on("data", function(data)
       {
