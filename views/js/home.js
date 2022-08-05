52ac69d63e49bc302d649ceb9b240cd1b4ca6d90
KaitoKunTatsu <88390464+KaitoKunTatsu@users.noreply.github.com>
2022-08-05 11:29:55 +0200

    homejs fix

--- views/js/home.js
+++ views/js/home.js
@@ -21,8 +21,8 @@ const newCarouselItems = (itemArray, active) => {
          if (itemArray[3*j+l] === undefined) break
          html += `
          <div class="col-lg-4 col-sm-12">
-          <button class="cbtn news-item" `/* action= "/item/`+ itemArray[3*j+l].ItemID + `*/`">
-            <div>s
+          <button class="cbtn news-item">
+            <div>
              <img src="images/Logo.png" class="article-img" alt="ItemImg">
               <h1 class="article-title">`+ itemArray[3*j+l].ItemName + `</h1>
               <p class="article-desc">` + itemArray[3*j+l].ShortDescription + `</p>
@@ -44,13 +44,13 @@ const categories = [`Products`, `Services`, `Bets`];
 
 recommendations.forEach((item, i) => {
 
-    htmlcode +=
+    htmlcode += 
     `
     <h1 class = "news-title">`+ `Top `+categories[i] + `</h1>
     <div id="carouselExampleControls`+ i +`" class="carousel slide" data-bs-ride="carousel">
       <div id="carousel-inner`+ i +`" class="carousel-inner">
       `
-    + newCarouselItems(item, true) +
+    + newCarouselItems(item, true) + 
       `
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls`+ i +`" data-bs-slide="prev">
@@ -66,24 +66,24 @@ recommendations.forEach((item, i) => {
    });
 
   $(".news-container").prepend(htmlcode);
-
+  
   $("#nextProducts").on("click", async () => {
     const lowestID = recommendations[0][recommendations[0].length-1].ItemID-1;
     const newProduct= await fetchData(`http://10.6.3.96:4242/item/recommendations/product/3/ignore/`+lowestID+`/desc`);
-    $("#carousel-inner0").append(newCarouselItems(newProduct, false));
+    $("#carousel-inner0").append(newCarouselItems(newProduct, false));  
     recommendations[0] = recommendations[0].concat(newProduct);
   });
   $("#nextServices").on("click", async () => {
       const lowestID = recommendations[1][recommendations[1].length-1].ItemID-1;
       const newServices = await fetchData(`http://10.6.3.96:4242/item/recommendations/service/3/ignore/`+lowestID+`/desc`);
-      $("#carousel-inner1").append(newCarouselItems(newServices, false));
+      $("#carousel-inner1").append(newCarouselItems(newServices, false));  
       recommendations[1] = recommendations[1].concat(newServices);
     });
   $("#nextBets").on("click", async () => {
     const lowestID = recommendations[3][recommendations[3].length-1].ItemID-1;
     const newBets = await fetchData(`http://10.6.3.96:4242/item/recommendations/bet/3/ignore/`+lowestID+`/desc`);
-    $("#carousel-inner3").append(newCarouselItems(newBets, false));
+    $("#carousel-inner3").append(newCarouselItems(newBets, false));  
     recommendations[3] = recommendations[3].concat(newBets);
-  });
+  });  
 }
 script()
