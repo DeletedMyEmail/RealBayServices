b091ee736a5fd7d6f2c334b77515e93911365eac
KaitoKunTatsu <88390464+KaitoKunTatsu@users.noreply.github.com>
2022-08-05 11:25:14 +0200

    db export && seasions on login && link to settings

--- app.js
+++ app.js
@@ -9,13 +9,21 @@ const {encryptString, hash} = require('./encryption.js')
 
 const apiAddress = "http://localhost:4242/";
 
+const fetchData = (url) => {
+    const data = fetch(`${url}`)
+      .then((request) => request.json())
+      .then((data) => data)
+  
+    return data
+}
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 app.use(express.static("views"));
 
+
 app.get('/', function(req, res) {
-    console.log(req.cookies);
     res.render("home.ejs");
 });
 
@@ -35,25 +43,23 @@ app.get('/itemSettings', function(req, res) {
     res.render("itemSettings.ejs");
 });
 
-app.get('/profileSettings', function(req, res) {
-  http.get(apiAddress+"user/1", function(responds)
-  {
-      responds.on("data", function(data)
-      {
-          const userData = JSON.parse(data);
-          if (!Object.keys(userData).length) res.render('errorpage.ejs',{error: "Couldn't find user"});
-          else if (userData) res.render("profileSettings.ejs", {Bio: userData.Bio, UserName: userData.UserName, Email: userData.Email, Twitter: userData.Twitter, Tel: userData.Tel, Bio: userData.Bio, LinkedIn: userData.LinkedIn});
-          else res.end()
-      });
-  });
+app.get('/profileSettings', async (req, res) => {
+    if (isNaN(req.cookies.SeasionID)) {
+        res.redirect("/login")
+        return
+    }
+    var userData = await query("SELECT UserName,Email,Location,Tel,Twitter,LinkedIn,Bio FROM User WHERE UserID=(SELECT UserID FROM Seasion WHERE SeasionID=?);",[parseInt(req.cookies.SeasionID)]);
+    userData = userData[0]
+    if (userData) {
+        res.render("profileSettings.ejs", {
+            Bio: userData.Bio, UserName: userData.UserName, Email: userData.Email, Twitter: userData.Twitter, 
+            Tel: userData.Tel, Bio: userData.Bio, LinkedIn: userData.LinkedIn
+        });
+    }    
+    else res.render('errorpage.ejs',{error: "Your seasion expired please sign in again"});
 });
 
-app.post("/login", function(req, res) {
-    
-})
-
 app.post("/userprofile", function(req, res) {
-    console.log(req.cookie)
     http.get(apiAddress+"user/"+req.body.searchInput, function(responds)
     {
         responds.on("data", function(data)
@@ -67,22 +73,25 @@ app.post("/userprofile", function(req, res) {
 });
 
 app.post("/login", async (req, res) => {
-    const inputPasswort = req.body.inputpasswort;
+    const inputPassword = req.body.inputpasswort;
     const inputEmail = req.body.inputemail;
-
-    const result = await query("SELECT UserName,UserID FROM User WHERE Email=? AND PwHash=?",[inputEmail, hash(inputPasswort)]); 
+    const result = await query("SELECT UserName,UserID FROM User WHERE Email=? AND PwHash=?",[inputEmail, hash(inputPassword)]); 
     
-    console.log(result)
-    if (result) console.log(1)
-    else console.log(0)
-
-    res.redirect("/profile");
+    if (result[0]) {
+        const newSeasionID = hash(new Date(Date.now())+result[0].UserID)
+        res.cookie('SeasionID', newSeasionID, 
+                { expires: new Date(Date.now() + 60000), httpOnly: false});
+        
+        await query("INSERT INTO Seasion VALUES(?,?)",[result[0].UserID, newSeasionID])
+        res.redirect("/");
+    }
+    else res.render('errorpage.ejs',{error: "Incorrect username or password"});
 });
 
 app.post("/register", function(req, res) {
   res.redirect("/profile");
 });
 
-app.listen(3001, function() {
+app.listen(3000, function() {
     console.log("Server online");
 });
--- realbay.sql
+++ realbay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 02. Aug 2022 um 11:11
+-- Erstellungszeit: 05. Aug 2022 um 09:15
 -- Server-Version: 10.4.24-MariaDB
 -- PHP-Version: 8.1.6
 
@@ -29,13 +29,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `item` (
   `ItemID` int(12) NOT NULL,
-  `MechantID` int(10) NOT NULL,
+  `MerchantID` int(10) NOT NULL,
   `ItemName` text NOT NULL,
   `TimeLimit` date DEFAULT NULL,
   `Stock` int(6) NOT NULL,
   `PricePerItem` double NOT NULL,
   `LongDescription` text NOT NULL,
   `ShortDescription` text NOT NULL,
+  `Category` text NOT NULL,
   `AvgRating` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -43,8 +44,27 @@ CREATE TABLE `item` (
 -- Daten für Tabelle `item`
 --
 
-INSERT INTO `item` (`ItemID`, `MechantID`, `ItemName`, `TimeLimit`, `Stock`, `PricePerItem`, `LongDescription`, `ShortDescription`, `AvgRating`) VALUES
-(1, 1, 'Ball', '2022-07-30', 1, 0, '', '', 10);
+INSERT INTO `item` (`ItemID`, `MerchantID`, `ItemName`, `TimeLimit`, `Stock`, `PricePerItem`, `LongDescription`, `ShortDescription`, `Category`, `AvgRating`) VALUES
+(1, 1, 'Ball', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'product', 10),
+(2, 1, 'Ball2', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'product', 10),
+(3, 1, 'Ball3', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'product', 10),
+(4, 1, 'Döner holen', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(5, 2, 'kickern', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'bet', 10),
+(6, 2, 'kickern', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'bet', 10),
+(7, 2, 'kickern', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'bet', 10),
+(8, 1, 'kochen', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(9, 1, 'Tee holen', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(10, 4, 'Pizza holen', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(11, 3, 'Linus boxen', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(12, 3, '3D Druck', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(13, 3, 'Logo Design', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(14, 3, 'Web Design', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(15, 3, 'IT Support', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(16, 3, 'Service 1', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(17, 3, 'Service 2', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(18, 3, 'Service 3', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(19, 3, 'Service 4', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'service', 10),
+(20, 2, 'Kickern gegen Praktis', '2022-07-30', 1, 0, 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'sfwfsfsfssfwfsfsfssfwfsfsfssfwfsfsfs', 'bet', 10);
 
 -- --------------------------------------------------------
 
@@ -102,6 +122,24 @@ CREATE TABLE `rating` (
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `seasion`
+--
+
+CREATE TABLE `seasion` (
+  `UserID` int(11) NOT NULL,
+  `SeasionID` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `seasion`
+--
+
+INSERT INTO `seasion` (`UserID`, `SeasionID`) VALUES
+(1, 4242);
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `user`
 --
 
@@ -123,7 +161,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`UserID`, `UserName`, `PwHash`, `Email`, `Location`, `Tel`, `Twitter`, `LinkedIn`, `Bio`) VALUES
 (1, 'LinusDerHuan', '.lkjhzsdertfzguhij', 'examplemail@mail.com', 'RealCoreGroup Essen, ETEC', '+49 0123455678', '@Linus_huan_der', 'Linus Huan', 'Moin, ich bin der Linus und nen Huan Moin, ich bin der Linus und nen Huan Moin, ich bin der Linus und nen Huan Moin, ich bin der Linus und nen Huan!'),
-(2, 'FranzWillhelm', 'fgjk34drt7zhui9lp', 'hallo@bye.tschüss', 'IndeinerMom', '', '', '', '');
+(2, 'FranzWillhelm', 'fgjk34drt7zhui9lp', 'hallo@bye.tschüss', 'IndeinerMom', '', '', '', ''),
+(3, 'Friedrich Mueller', 'dashjj-gesd!', 'friedrich@mueller.de', 'ETEC ', '+49 12344323', 'firdrick__1999', 'Mueller.Friedrich', ' Tach ich bin der Friedrich Tach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der FriedrichTach ich bin der Friedrich'),
+(4, 'Paul Schmidts', 'da-fdadwwß', 'pailischmauli@examplemail.com', 'ETEC', '+49 130423872', 'Paul._.Schmidts', 'Paul Schmidts', 'Paul Schmidts Paul Schmidts vPaul SchmidtsPaul SchmidtsPaul SchmidtsPaul SchmidtsPaul SchmidtsPaul SchmidtsPaul SchmidtsPaul Schmidts');
 
 --
 -- Indizes der exportierten Tabellen
@@ -134,7 +174,7 @@ INSERT INTO `user` (`UserID`, `UserName`, `PwHash`, `Email`, `Location`, `Tel`, 
 --
 ALTER TABLE `item`
   ADD PRIMARY KEY (`ItemID`),
-  ADD KEY `MechantID` (`MechantID`);
+  ADD KEY `MechantID` (`MerchantID`);
 
 --
 -- Indizes für die Tabelle `order`
@@ -159,6 +199,12 @@ ALTER TABLE `rating`
   ADD KEY `UserID` (`UserID`);
 
 --
+-- Indizes für die Tabelle `seasion`
+--
+ALTER TABLE `seasion`
+  ADD PRIMARY KEY (`SeasionID`,`UserID`);
+
+--
 -- Indizes für die Tabelle `user`
 --
 ALTER TABLE `user`
@@ -172,7 +218,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `item`
 --
 ALTER TABLE `item`
-  MODIFY `ItemID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ItemID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT für Tabelle `order`
@@ -184,7 +230,7 @@ ALTER TABLE `order`
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints der exportierten Tabellen
@@ -194,7 +240,7 @@ ALTER TABLE `user`
 -- Constraints der Tabelle `item`
 --
 ALTER TABLE `item`
-  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`MechantID`) REFERENCES `user` (`UserID`);
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`MerchantID`) REFERENCES `user` (`UserID`);
 
 --
 -- Constraints der Tabelle `order`
